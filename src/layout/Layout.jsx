5ec1a24119a328cbbf048eb1ed9598e73a72b2e6
rudrapatel51 <rudrapokar5105@gmail.com>
2024-08-13 19:17:35 +0530

    loader implement

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Outlet, Link } from "react-router-dom";
 import Navbar from '../Components/navbar/Navbar';
+import Loader from '../loader/Loader';
 
 
 const Layout = () => {
+    const [isLoading, setIsLoading] = useState(true);
+
+    setTimeout(() => {
+        setIsLoading(false);
+    }, 2000);
     return (
         <div>
-            <Navbar />
-            <Outlet />
+            {isLoading ? (
+                <Loader />
+            ) : (
+                <div>
+                    <Navbar />
+                    <Outlet />
+                </div>
+            )}
         </div>
     )
 }
