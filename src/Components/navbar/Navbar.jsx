06a1a2381f2eecb8d007e47940903208b951e958
rudrapatel51 <rudrapokar5105@gmail.com>
2024-09-19 21:07:14 +0530

    update build

--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
                             </NavLink>
                         </li>
                         <li>
-                            <NavLink
+                           <NavLink
                                 to="/shop"
                                 activeClassName="active"
                                 className="block py-2 px-3 text-inherit rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0"
