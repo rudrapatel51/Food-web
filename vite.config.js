8259e4648d371a36849a6b2a39e0d8b64baf48eb
rudrapatel51 <rudrapokar5105@gmail.com>
2024-09-19 21:22:20 +0530

    vite config and js file of cartslice

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@redux': resolve(__dirname, 'src/redux'), 
+    },
+  },
+});
