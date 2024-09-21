f03972c74cf717959cd8551db0fdd95382e8e4cc
rudrapatel51 <rudrapokar5105@gmail.com>
2024-09-21 13:23:55 +0530

    make changes in main.jsx file of import error

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from './cartSlice';
+import cartReducer from "../redux/cartSlice"
 
 const store = configureStore({
   reducer: {
