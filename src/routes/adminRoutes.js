f806268a701c1035c620b3968935eebb7e2e6541
rudrapatel51 <rudrapokar5105@gmail.com>
2025-02-05 11:01:32 +0530

    order changes

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import Order from '../models/Order.js'
+import Order from '../models/order.js'
 import verifyToken from '../middleware/authMiddleware.js'
 
 const router = express.Router();
